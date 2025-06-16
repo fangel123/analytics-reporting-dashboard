d486428ebd3b9ba4f5d4affa0e8bd175a2cc6d94
fangel123 <mikhajuntaq@gmail.com>
2025-06-17 03:48:01 +0700

    fix: Listen on all interfaces for deployment

--- backend/index.js
+++ backend/index.js
@@ -58,6 +58,6 @@ app.get("/api/sales/heatmap-by-state", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Backend server berjalan di http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is listening on port http://localhost:${port}`);
 });
