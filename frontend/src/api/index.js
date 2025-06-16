41cd5c4a55ea0b487114b3b031f34a83d5385da9
fangel123 <mikhajuntaq@gmail.com>
2025-06-17 03:31:13 +0700

    fix(api): Force use of VITE_API_URL for production

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001/api";
+const API_URL = import.meta.env.VITE_API_URL
 
 export const fetchProductRanking = () =>
   axios.get(`${API_URL}/products/ranking`);
